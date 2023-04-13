d18d45a843cc94b1d7a7149f86717157d171715e
yejunyu <liuxing_yue@126.com>
2023-04-13 22:26:45 +0800

    1

--- src/main/java/com/yejunyu/seq/Seq.java
+++ src/main/java/com/yejunyu/seq/Seq.java
@@ -1,8 +1,8 @@
 package com.yejunyu.seq;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Stream;
 
 /**
  * @Author yjy
@@ -70,8 +70,82 @@ public interface Seq<T> {
     }
 
     default Seq<T> drop(int n) {
-        return c->{
+        return c -> {
+            int[] a = {n};
+            consume(t -> {
+                if (a[0] <= 0) {
+                    c.accept(t);
+                } else {
+                    a[0]--;
+                }
+            });
+        };
+    }
 
-        }
+    /**
+     * 对流的某个元素添加一个操作consumer，但是不执行流——对应Stream.peek。
+     *
+     * @param consumer
+     * @return
+     */
+    default Seq<T> onEach(Consumer<T> consumer) {
+        return c -> consume(consumer.andThen(c));
+    }
+
+    default <E, R> Seq<R> zip(Iterable<E> iterable, BiFunction<T, E, R> function) {
+        return c -> {
+            Iterator<E> iterator = iterable.iterator();
+            consumeTillStop(t -> {
+                if (iterator.hasNext()) {
+                    c.accept(function.apply(t, iterator.next()));
+                } else {
+                    stop();
+                }
+            });
+        };
+    }
+
+    /**
+     * 终端操作
+     *
+     * @param separate
+     * @return
+     */
+    default String join(String separate) {
+        var joiner = new StringJoiner(separate);
+        consume(t -> joiner.add(t.toString()));
+        return joiner.toString();
+    }
+
+    default List<T> toList() {
+        List<T> list = new ArrayList<>();
+        consume(list::add);
+        return list;
+    }
+
+    /**
+     * 下划线转驼峰
+     *
+     * @param str
+     * @return
+     */
+    static String underscoreToCamel(String str) {
+        UnaryOperator<String> capitalize = s -> s.substring(0, 1).toUpperCase();
+        // 利用生成器构造一个方法的流
+        Seq<UnaryOperator<String>> seq = c -> {
+            c.accept(String::toLowerCase);
+            while (true) {
+                c.accept(capitalize);
+            }
+        };
+        List<String> split = Arrays.asList(str.split("_"));
+        return seq.zip(split, Function::apply).join("");
+    }
+
+    public static void main(String[] args) {
+        Stream<Integer> steam = Stream.of(1, 2, 3);
+        Seq<Integer> seq = steam::forEach;
+        List<Integer> integers = seq.map(m -> m + 1).toList();
+        System.out.println(integers);
     }
 }
