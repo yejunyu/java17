e7d9b4b699e3f47a228b66ab964fb4b55e7c1aff
yejunyu <liuxing_yue@126.com>
2023-06-15 22:39:57 +0800

    spring set注入和构造器注入

---
+++ spring6/src/main/java/com/yejunyu/dao/UserDao.java
@@ -0,0 +1,18 @@
+package com.yejunyu.dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @Author yjy
+ * @Description //TODO
+ * @Date 2023/6/13
+ **/
+public class UserDao {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public void insert() {
+        logger.info("user dao insert");
+    }
+}
---
+++ spring6/src/main/java/com/yejunyu/testCase/User.java
@@ -0,0 +1,12 @@
+package com.yejunyu.testCase;
+
+/**
+ * @Author yjy
+ * @Description //TODO
+ * @Date 2023/6/13
+ **/
+public class User {
+    public User() {
+        System.out.println("user init!");
+    }
+}
